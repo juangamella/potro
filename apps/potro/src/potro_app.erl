695dffd06f6dde06af45107aa96b4749fa085dc5
Juan Luis Gamella Martín <juangamella@gmail.com>
2017-05-28 19:36:32 +0200

    Launch cowboy listener

--- apps/potro/src/potro_app.erl
+++ apps/potro/src/potro_app.erl
@@ -14,12 +14,21 @@
 %% API
 %%====================================================================
 
-start(_StartType, _StartArgs) ->
-    potro_sup:start_link().
+start(_StartType, StartArgs) ->
+  %% Start cowboy
+  PoolSize = proplists:get_value(pool_size, StartArgs, 100),
+  Port = proplists:get_value(port, StartArgs, 8080),
+  Dispatch = cowboy_router:compile(potro_routes:routes()),
+  {ok, _} = cowboy:start_clear(my_http_listener,
+                               PoolSize,
+                               [{port, Port}],
+                               #{env => #{dispatch => Dispatch}}
+                              ),
+  potro_sup:start_link().
 
 %%--------------------------------------------------------------------
 stop(_State) ->
-    ok.
+  ok.
 
 %%====================================================================
 %% Internal functions
